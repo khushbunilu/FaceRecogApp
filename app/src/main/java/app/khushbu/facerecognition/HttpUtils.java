c297aa0e86ab0ca9c81097b7bd2aef36d25a30e2
sachinnair92 <sachinnair92@gmail.com>
2016-06-19 09:11:16 +0530

    first commit

--- app/src/main/java/app/khushbu/facerecognition/HttpUtils.java
+++ app/src/main/java/app/khushbu/facerecognition/HttpUtils.java
@@ -8,7 +8,9 @@ import com.loopj.android.http.RequestParams;
  * Created by khushbu on 31/5/16.
  */
 public class HttpUtils {
-    private static final String BASE_URL = "http://10.42.0.193:5000/api/";
+    private static final String BASE_URL = "https://khush-rest-server.herokuapp.com/api/";
+
+    //private static final String BASE_URL = "http://10.42.0.193:5000/api/";
 
 
     private static AsyncHttpClient client = new AsyncHttpClient();
